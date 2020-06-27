820b9e8ac853ea62ead6f53fb7132430fec7b0f6
sandorprog <toth.sandor.prog@gmail.com>
2020-06-27 14:08:26 +0200

    Init page.

---
+++ js/java.js
@@ -0,0 +1,18 @@
+
+function calcAmaunt() {
+    let n1 = document.querySelector("select[name='pizzaPrice']");
+    let n2 = document.querySelector("select[name='extraPrice']");
+    let amauntInput = document.querySelector("input[name='order']");
+    let amauont = parseInt(amauntInput.value) * parseInt(n1.value) + parseInt(n2.value);
+    let showAmaunt = document.querySelector("span.show");
+    showAmaunt.innerHTML = amauont;
+}
+
+function calcBuy() {
+
+
+    let n3 = document.querySelector("input[name='persName']");
+    let n4 = document.querySelector("input[name='persAddres']");
+    alert("Szállítási adatok Név: " + n3.value + " Cím: " + n4.value + " Fzetendő:   Ft"
+        + " Köszönjük rendelését! ");
+}
